14c760f45fc088d6b52ee93c3d9db73d2f9b2c9a
VladimriSt <100336166+VladimriSt@users.noreply.github.com>
2022-03-24 11:05:51 +0300

    Update Forms_List.java

--- src/forms/pack/Forms_List.java
+++ src/forms/pack/Forms_List.java
@@ -1,65 +1,13 @@
 package forms.pack;
-public class Forms_List{
-	
-	Form form;
-	Point point;
-	Segment segment;
-	Rectangle rectangle;
-	Square square;
-	Ellipse ellipse;
-	Circle circle;
-	
-	private Form getForm() {
-		return form;
+public class Forms_List{	
+	typedef struct FormLst{
+		Form form;
+		Point point;
+		Segmet segment;
+		Rectangle rectangle;
+		Square square;
+		Ellipse ellipse;
+		Circle circle;
+		struct FormLst* next;
 	}
-	private void setForm(Form form) {
-		this.form = form;
-	}
-	private Point getPoint() {
-		return point;
-	}
-	private void setPoint(Point point) {
-		this.point = point;
-	}
-	private Segment getSegment() {
-		return segment;
-	}
-	private void setSegment(Segment segment) {
-		this.segment = segment;
-	}
-	private Rectangle getRectangle() {
-		return rectangle;
-	}
-	private void setRectangle(Rectangle rectangle) {
-		this.rectangle = rectangle;
-	}
-	private Square getSquare() {
-		return square;
-	}
-	private void setSquare(Square square) {
-		this.square = square;
-	}
-	private Ellipse getEllipse() {
-		return ellipse;
-	}
-	private void setEllipse(Ellipse ellipse) {
-		this.ellipse = ellipse;
-	}
-	private Circle getCircle() {
-		return circle;
-	}
-	private void setCircle(Circle circle) {
-		this.circle = circle;
-	}
-	
-	public Forms_List(Form form, Point point, Segment segment, Rectangle rectangle, Square square, Ellipse ellipse, Circle circle) {
-		this.form = form;
-		this.point = point;
-		this.segment = segment;
-		this.rectangle = rectangle;
-		this.square = square;
-		this.ellipse = ellipse;
-		this.circle = circle;
-	}
-
 }
